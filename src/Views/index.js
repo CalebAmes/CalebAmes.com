36819abb44fb12940f2fecd2d80e57c292e5320e
Caleb Gilbert <clutchcaleb@gmail.com>
2021-08-16 13:18:54 -0400

    boxOne

---
+++ src/Views/index.js
@@ -0,0 +1,17 @@
+import './index.css';
+
+const Home = () => {
+
+  return (
+    <>
+      <div className='boxOne'>
+        <div className='boxOneName'>
+          <h3>Caleb Ames Gilbert</h3>
+        </div>
+      </div>
+      
+    </>
+  )
+}
+
+export default Home;
